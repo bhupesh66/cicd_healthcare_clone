ff8564c8a41b1400cc1bfd6bb7cb72ea725add5a
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 14:23:56 +0200

    airflow test

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print("hello world")
 print("dev branch has been successfully build")
+print("airflow infra dockerlized  ,cloudwatch infra ,azure function infra and azurefunction and adlsgen2 infra")
