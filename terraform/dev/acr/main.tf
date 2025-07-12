07983a093eff867aa01201b464fa06ade6f7b02d
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 14:27:42 +0200

    airflow test

--- terraform/dev/acr/main.tf
+++ terraform/dev/acr/main.tf
@@ -17,15 +17,15 @@ module "network" {
 }
 
 module "container_instance" {
-  source              = "../../modules/container_instance"
-  prefix              = var.prefix
-  location            = var.location
- resource_group_name = data.azurerm_resource_group.rg.name
-  acr_login_server    = module.acr.acr_login_server
-  docker_image        = var.docker_image
-  image_tag           = var.image_tag
-  environment_variables =var.environment_variables
-  tags                = var.tags
+  source                = "../../modules/container_instance"
+  prefix                = var.prefix
+  location              = var.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  acr_login_server      = module.acr.acr_login_server
+  docker_image          = var.docker_image
+  image_tag             = var.image_tag
+  environment_variables = var.environment_variables
+  tags                  = var.tags
 }
 
 output "aci_fqdn" {
