36eb3c57266e46042f6d2bbe42cfe51fd4816e5f
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 21:45:34 +0200

    hey

--- terraform/modules/acr/output.tf
+++ terraform/modules/acr/output.tf
@@ -1,11 +1,10 @@
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "Login server URL for ACR"
+  value       = azurerm_container_registry.acr.login_server
 }
 
 output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
+  description = "Admin username for ACR"
+  value       = azurerm_container_registry.acr.admin_username
 }
 
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-}
