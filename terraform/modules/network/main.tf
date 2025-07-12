bf4532e0f7b0e30f3bea841efd06da586b6746af
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 14:21:26 +0200

    trying for airflow

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
