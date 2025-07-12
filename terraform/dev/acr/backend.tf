0fadcc3702832a137f81d70231a0c14e2c70d761
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 15:33:27 +0200

    try again

--- terraform/dev/acr/backend.tf
+++ terraform/dev/acr/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstate147025af"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"  # Name of the state file in blob storage
+    key                  = "terraform.tfstate" # Name of the state file in blob storage
   }
 }
