e3c558fe9ef27c8bcc9d7cf0c0794bd906017cd5
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 20:06:43 +0200

    trying

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -11,15 +11,15 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+#   subscription_id = var.azure_subscription_id
+#   client_id       = var.azure_client_id
+#   client_secret   = var.azure_client_secret
+#   tenant_id       = var.azure_tenant_id
 }
 
 resource "azurerm_resource_group" "tfstate" {
   name     = "tfstate-rg"
-  location = "Sweden Central" # Choose your region
+  location = "Sweden Central"  # Choose your region
 }
 
 resource "azurerm_storage_account" "tfstate" {
