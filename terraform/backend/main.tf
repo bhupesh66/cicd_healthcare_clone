b098dd72ba83bcc0a2254218495d08aa6a830c6d
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-12 15:12:24 +0200

    remote backend has been build

---
+++ terraform/backend/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+   subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate-rg"
+  location = "Sweden Central"  # Choose your region
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_id.unique.hex}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+resource "random_id" "unique" {
+  byte_length = 4
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.tfstate.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.tfstate.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.tfstate.name
+}
