c09c205d14f45b67f58e6f160c43e2c3f7eaa098
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-14 02:13:52 +0200

    airflow acr tested

--- terraform/modules/container_instance/main.tf
+++ terraform/modules/container_instance/main.tf
@@ -22,9 +22,18 @@ resource "azurerm_container_group" "aci" {
       protocol = "TCP"
     }
 
-   commands = ["airflow", "webserver", "--port", "8080"]
+    commands = ["airflow", "webserver", "--port", "8080"]
 
-    environment_variables = var.environment_variables
+    environment_variables = {
+      AIRFLOW__CORE__EXECUTOR      = "SequentialExecutor"
+      AIRFLOW__CORE__SQL_ALCHEMY_CONN = "sqlite:////root/airflow/airflow.db"
+      # Add any other required env vars here, for example:
+      # AIRFLOW__WEBSERVER__WEB_SERVER_PORT = "8080"
+      # AIRFLOW__API__AUTH_BACKENDS = "airflow.api.auth.backend.basic_auth"
+      # AIRFLOW__CORE__FERNET_KEY = "your_fernet_key_here"
+      # AIRFLOW_VAR_ENV = "dev"
+      # ...
+    }
   }
 
   image_registry_credential {
