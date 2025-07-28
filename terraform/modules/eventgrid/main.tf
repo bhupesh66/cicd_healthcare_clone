33664af210723a55d51fbf5928a647030e9b71a7
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-28 23:39:46 +0200

    event grid has been activated

--- terraform/modules/eventgrid/main.tf
+++ terraform/modules/eventgrid/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_eventgrid_event_subscription" "file_sub" {
 
 # Diagnostics (remove unsupported StorageRead logs)
 resource "azurerm_monitor_diagnostic_setting" "eventgrid_diagnostics" {
-  name                       = "eventgrid-logs"
+  name                       = "eventgrid-subscription-logs"
   target_resource_id         = azurerm_eventgrid_event_subscription.file_sub.id
   log_analytics_workspace_id = var.log_analytics_workspace
 
