0321ede052a5b288eed7afe242cf6158acc17d0b
bhupes <bhupesh.aawasthi952@gmail.com>
2025-07-31 04:51:12 +0200

    ss

--- terraform/dev/infra/output.tf
+++ terraform/dev/infra/output.tf
@@ -2,6 +2,5 @@ output "function_app_id" {
   value = module.function.function_endpoint
 }
 output "function_app_name" {
-  value       = module.function.azurerm_function_app.func.name
-  description = "The name of the Azure Function App"
+  value = module.function.function_app_name
 }
