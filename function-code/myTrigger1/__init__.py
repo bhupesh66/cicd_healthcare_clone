899cb5af1ca0137e5f3ed14849f1301ed6201249
bhupes <bhupesh.aawasthi952@gmail.com>
2025-08-02 19:10:05 +0200

    shuld work

--- function-code/myTrigger1/__init__.py
+++ function-code/myTrigger1/__init__.py
@@ -120,6 +120,11 @@ def main(event: func.EventGridEvent):
         blob_client = BlobServiceClient.from_connection_string(STORAGE_CONN)
         container_client = blob_client.get_container_client(CONTAINER_NAME)
 
+        existing_blobs = list(container_client.list_blobs(name_starts_with="incoming/dassscrub/"))
+        existing_blob_names = [blob.name for blob in existing_blobs]
+
+        logging.info(f"Found {len(existing_blob_names)} blobs under 'incoming/dassscrub/': {existing_blob_names}")
+
         missing = []
         for path in dependencies:
             full_path = f"incoming/dassscrub/{path}"
