9fa72e03b7717c27f1158734365390313f7b2ab7
bhupes <bhupesh.aawasthi952@gmail.com>
2025-08-03 02:15:55 +0200

    deploying

--- terraform/dev/infra/main.tf
+++ terraform/dev/infra/main.tf
@@ -54,7 +54,7 @@ module "function" {
   function_name             = var.function_name
   function_storage_account  = var.function_storage_account
   log_analytics_workspace   = module.log_analytics.workspace_id
-  storage_account_id      = module.storage.storage_account_id
+  storage_account_id        = module.storage.storage_account_id
   storage_connection_string = module.storage.storage_connection_string
   # Removed servicebus_connection_string and servicebus_queue_name
 }
